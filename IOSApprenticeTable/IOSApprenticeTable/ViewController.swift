65045922e003fec4a4059d854318b665ef1992da
Thien Tran <thien.tran@dfo.global>
2020-03-16 23:01:38 +0700

    Init project

---
+++ IOSApprenticeTable/IOSApprenticeTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOSApprenticeTable
+//
+//  Created by Thien Tran on 3/16/20.
+//  Copyright © 2020 Thien Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
